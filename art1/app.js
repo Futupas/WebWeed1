bcbaa40a159482e1d402ccf70af72241c32ce779
Olexandr Panov <futupas@gmail.com>
2020-06-13 02:57:30 +0300

    no bugs

--- art1/app.js
+++ art1/app.js
@@ -9,6 +9,8 @@ const ACCURACY = 1000 * 1000 * 1000;
 let fieldSegments = [];
 /** @type {Array<LineSegment>} */
 let figureSegments = [];
+/** @type {Array<LineSegment>} */
+let raySegments = [];
 let path;
 
 class LineSegment {
@@ -110,7 +112,10 @@ class LineSegment {
             return false;
         }
         
-        if (k1 === k2) return false; // segments are parallel
+        if (Math.round((k1 - k2) * ACCURACY) == 0) return false; // segments are parallel
+
+        if (Math.round(k1 * ACCURACY) == 0) intersectionY = (this.y1 + this.y2) / 2; // 'this' is horizontal
+        if (Math.round(k2 * ACCURACY) == 0) intersectionY = (lineSegment.y1 + lineSegment.y2) / 2; // 'lineSegment' is horizontal
 
         if (intersectionX < Math.min(this.x1, this.x2) ||
             intersectionX > Math.max(this.x1, this.x2) ||
@@ -129,7 +134,32 @@ class LineSegment {
 
 }
 
+/**
+ * draws a field with n (quantityOfSides) sides
+ * @param {HTMLElement} svg svg element, where to draw the line
+ * @param {number} quantityOfSides quantity of sIdes
+ */
+function drawHiddenField(quantityOfSides) {
+    let shiftAngle = Math.PI / quantityOfSides;
+    let radius = FIELD_RADIUS / Math.cos(shiftAngle);
+    let prevXCoord = radius*Math.cos(shiftAngle);
+    let prevYCoord = radius*Math.sin(shiftAngle);
+    // fieldSegments = [];
+    for (let i = 1; i < quantityOfSides; i-=-1) {
+        let angle = shiftAngle + 2*Math.PI/quantityOfSides*i;
+        let xCoord = radius*Math.cos(angle);
+        let yCoord = radius*Math.sin(angle);
+        let line = new LineSegment(prevXCoord, prevYCoord, xCoord, yCoord);
+        prevXCoord = xCoord;
+        prevYCoord = yCoord;
+        fieldSegments.push(line);
+        // line.draw(mainsvg, '#00f', '3px');
+    }
+    let line = new LineSegment(prevXCoord, prevYCoord, radius*Math.cos(shiftAngle), radius*Math.sin(shiftAngle));
+    fieldSegments.push(line);
+    // line.draw(mainsvg, '#00f', '3px');
 
+}
 
 
 /**
@@ -156,7 +186,7 @@ function drawField(svg, quantityOfSides, strokeColor, strokeWidth) {
     let line = new LineSegment(prevXCoord, prevYCoord, FIELD_RADIUS*Math.cos(0), FIELD_RADIUS*Math.sin(0));
     fieldSegments.push(line);
     line.draw(svg, strokeColor, strokeWidth);
-
+    drawHiddenField(quantityOfSides);
 }
 
 
@@ -172,13 +202,19 @@ function drawAPathSegment(x, y, angle) {
     let fieldIntersects = false;
     let ray = new LineSegment(x, y, x+3*FIELD_RADIUS*Math.cos(angle), y+3*FIELD_RADIUS*Math.sin(angle));
     // ray.draw(document.getElementById('mainsvg'), '#0f0', '1px');
+    raySegments.push(ray);
     for (let i = 0; i < fieldSegments.length; i-=-1) {
         let intersection = fieldSegments[i].getCoordsOfIntersection(ray);
         if (intersection !== false) {
             let r = Math.sqrt(Math.pow(x - intersection[0], 2) + Math.pow(y - intersection[1], 2));
             if (r > 1/ACCURACY) {
-                closestIntersection = intersection;
-                fieldIntersects = fieldSegments[i];
+                if (closestIntersection === false) {
+                    closestIntersection = intersection;
+                    fieldIntersects = fieldSegments[i];
+                } else if (r < Math.sqrt(Math.pow(x - closestIntersection[0], 2) + Math.pow(y - closestIntersection[1], 2))) {
+                    closestIntersection = intersection;
+                    fieldIntersects = fieldSegments[i];
+                }
             }
         }
     }
@@ -221,7 +257,7 @@ let currentNumber = 3;
 
 
 drawField(document.getElementById('mainsvg'), currentNumber, '#000', '5px');
-drawPath(document.getElementById('mainsvg'), 100, 0, 0, .1*Math.PI, '#f00', '1px');
+drawPath(document.getElementById('mainsvg'), 100, 0, 0, .3, '#f00', '1px');
 
 window.onkeydown = function(e) {
     if (e.key == 'ArrowLeft') currentNumber --;
@@ -229,12 +265,39 @@ window.onkeydown = function(e) {
     this.console.log(currentNumber);
     mainsvg.innerHTML = '';
     drawField(document.getElementById('mainsvg'), currentNumber, '#000', '5px');
-    drawPath(document.getElementById('mainsvg'), 100, 0, 0, .1*Math.PI, '#f00', '1px');
+    drawPath(document.getElementById('mainsvg'), 1000, 0, 0, .1234567876545678909876543, '#f00', '1px');
 }
 
 
-// let line1 = new LineSegment(-180.19377358048382, 86.77674782351164, -180.19377358048382, -86.7767478235116);
+// let line1 = new LineSegment(190.2113032590307, 61.803398874989476, 178.20130483767358, 90.79809994790935);
+// // let line1 = new LineSegment(197.53766811902756, 31.286893008046174, 190.2113032590307, 61.803398874989476);
 // line1.draw(mainsvg, '#000', '5px');
-// let line2 = new LineSegment(-31.964374447409337, -192.12345209880309, -339.70394089095254, 322.9458240623169);
+// let line2 = new LineSegment(0, 0, 570.6339097770921, 185.41019662496845);
 // line2.draw(mainsvg, '#0f0', '1px');
 // console.log(line1.getCoordsOfIntersection(line2));
+
+/*
+x1: 0
+x2: 570.6339097770921
+y1: 0
+y2: 185.41019662496845
+ray
+
+
+
+line 1
+x1: 197.53766811902756
+x2: 190.2113032590307
+y1: 31.286893008046174
+y2: 61.803398874989476
+
+
+line 2
+x1: 190.2113032590307
+x2: 178.20130483767358
+y1: 61.803398874989476
+y2: 90.79809994790935
+
+
+
+*/
